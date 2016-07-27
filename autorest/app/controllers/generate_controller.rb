c2a3671eacc172429547de486cdf588a0ede11d3
veronicagg <vergiau@users.noreply.github.com>
2016-07-26 18:29:32 -0700

    my changes

--- autorest/app/controllers/generate_controller.rb
+++ autorest/app/controllers/generate_controller.rb
@@ -1,4 +1,33 @@
 class GenerateController < ApplicationController
   def client
+
+    autorest_version ='0.17.0-Nightly20160724'
+    spec_url = 'https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/arm-compute/2016-03-30/swagger/compute.json'
+    add_credentials = true
+    client_name = "ClientName"
+    code_generator = "Ruby"
+    header = "MICROSOFT_MIT"
+    modeler = "Swagger"
+    namespace = "Namespace"
+    package_name = "PackageName"
+    package_version = "0.1.0"
+    payload_flattening_threshold = 2
+    validation_level = "3"
+
+    cmd_line_arguments = {
+        '-Input': spec_url,
+        '-Namespace': namespace,
+        '-OutputDirectory': '.',
+        '-CodeGenerator': code_generator,
+        '-Modeler': modeler,
+        '-ClientName': client_name,
+        '-PaylodFlatteningThreshold': payload_flattening_threshold,
+        '-Header': header,
+        '-AddCredentials': add_credentials,
+        '-OutputFileName': '',
+        '-pn': package_name,
+        '-pv': package_version
+    }
+    GenerateHelper.execute_autorest(autorest_version, cmd_line_arguments)
   end
 end
--- autorest/app/helpers/generate_helper.rb
+++ autorest/app/helpers/generate_helper.rb
@@ -1,2 +1,38 @@
+require 'open3'
 module GenerateHelper
+
+  def self.execute_autorest(autorest_version, cmd_line_arguments)
+    args = cmd_line_arguments.map{|k,v| "#{k} #{v.to_s}" unless (v.nil? or v.to_s.empty?)}.join(' ')
+    puts "AutoRest Arguments: #{args}"
+    puts "Installing AutoRest version: #{autorest_version}"
+    system("nuget install AutoRest -Source https://www.myget.org/F/autorest/ -pre -OutputDirectory /output/ -Version #{autorest_version}")
+
+
+    puts "Generating code..."
+    # output_message = `mono AutoRest.*/tools/AutoRest.exe #{args}`
+    # exit_code = $?.exitstatus
+    # exit_error = $?
+    # puts "Output: #{output_message}"
+    # puts "Exit code #{exit_code}"
+    # puts "Exit error #{exit_error}" unless exit_code == 0
+
+    cmd = "mono AutoRest.*/tools/AutoRest.exe #{args}"
+    Open3.popen2e(cmd) do |_, stdout_err, wait_thr|
+      message = ""
+      while line = stdout_err.gets
+        message = message + line
+        puts line
+      end
+
+      exit_status = wait_thr.value #remove pid from exit status
+      # code = $?.exitstatus
+      puts "Exit code: #{exitstatus}"
+      unless exit_status.success?
+        abort "FAILED !!!"
+      end
+    end
+
+    #e = Error new(exit_status, message)
+
+  end
 end
